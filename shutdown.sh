77b59b2d7585e013d97c381c4139133e22d84b58
NDokeh <ndokeh@gmail.com>
2015-08-18 21:47:25 -0400

    Update shutdown.sh
    Removed whitespace

--- shutdown.sh
+++ shutdown.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 #The following is a shutdown script for linux systems
 #Shutdown will commence at 10 minutes (-g600), it can be set for 5 minutes(-g300) or 15 minutes (-g900)
-
 shutdown -i5 -g600 -y "system will go down in 10 minutes"
-
 sleep 300
-
 wall System will go down in 5 minutes. Drop whatever you're doing and save your work.
-
 sleep 180
-
 wall Final One minute warning: System will shut down in one minute.
