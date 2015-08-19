57d8b38fb5a800c0860cb46346d632f622a2c583
NDokeh <ndokeh@gmail.com>
2015-08-18 21:23:57 -0400

    Update backup.sh
    Removed whitespace

--- backup.sh
+++ backup.sh
@@ -2,12 +2,8 @@
 #A basic backup script
 #home location may vary by Linux OS
 #Manually move all backups to a external hard drive or usb stick if possible
-
 cp /home/[insertfolders] /home/backups
-
 sleep 3
-
 gzip /home/backups/[insertfolders].old
-
 tar /home/backups/[insertfolders].old
 
