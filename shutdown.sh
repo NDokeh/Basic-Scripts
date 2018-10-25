1220818a436043bff01ab74ba3748c97d854652a
Ndubuisi Okeh <ndokeh@gmail.com>
2018-10-25 15:27:33 -0400

    Update shutdown.sh
    Improved script

--- shutdown.sh
+++ shutdown.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
 #The following is a shutdown script for linux systems
-#Shutdown will commence at 10 minutes (-g600), it can be set for 5 minutes(-g300) or 15 minutes (-g900)
-shutdown -i5 -g600 -y "system will go down in 10 minutes"
+
+#Shutdown will commence at 10 minutes (+10), it can be set for 5 minutes(+5) or 15 minutes (+15)
+
+shutdown -r +10 "system will go down in 10 minutes"
+
 sleep 300
-wall System will go down in 5 minutes. Drop whatever you're doing and save your work.
-sleep 180
+
+wall System will go down in 5 minutes. Drop whatever you're doing and save 
+your work.
+
+sleep 240
+
 wall Final One minute warning: System will shut down in one minute.
