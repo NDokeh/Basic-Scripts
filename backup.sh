74fd0259cbd03012bc5cfabf8c0e1b8c53aad22d
Ndubuisi Okeh <ndokeh@gmail.com>
2018-10-25 15:30:38 -0400

    Update backup.sh
    Updated script with timestamping of backup tars with moving to a dedicated backup folder for later extraction.

--- backup.sh
+++ backup.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
 #A basic backup script
-#home location may vary by Linux OS
+
+#The script basically basically copies the files from my home folder into a seperate folder within my home folder balled "backups"
+
+#This is not recommneded if I had video or music files unless they're removed from the backup folder via a external hard drive or usb stick after I'm done with the machine for the day. This method can be used if you only have actual documents like textfiles or word documents.
+
 #Manually move all backups to a external hard drive or usb stick if possible
-cp /home/[insertfolders] /home/backups
-sleep 3
-gzip /home/backups/[insertfolders].old
-tar /home/backups/[insertfolders].old
+
+BACKUP_SOURCE=/home/jkid
+
+BACKUP_DEST=/home/jkid/backups
+
+DATE=date "+%y-%m-%d"
+
+tar czf jkid.tar.gz $BACKUP_SOURCE
+
+mv jkid.tar.gz $BACKUP_DEST
+
+mv jkid.tar.gz jkid_$DATE.tar.gz
