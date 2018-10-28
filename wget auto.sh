a1b14dad54b3c6f130dc3450ba55cef88af138e6
Ndubuisi Okeh <ndokeh@gmail.com>
2018-10-28 16:37:40 -0400

    Create wget auto.sh

---
+++ wget auto.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#A basic script for automating downloads of a webpage, outputting the 
+
+result in text and doing it on the background
+
+#replace the [[]] and the words insidewith the website and website 
+
+name.
+
+#I do not recommend doing more than 2 urls at a time.
+
+wget -q -O [website] [websitename].txt 2>&1 &
+
+wget -q -O [website] [websitename].txt 2>&1 &
